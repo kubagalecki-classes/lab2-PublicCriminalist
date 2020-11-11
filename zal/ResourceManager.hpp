8197a8eb8d20ba217892ea9fc53c75c15817f95b
PublicCriminali <01123869@pw.edu.pl>
2020-11-11 21:29:10 +0000

    Rozw 2

--- zal/ResourceManager.hpp
+++ zal/ResourceManager.hpp
@@ -2,13 +2,34 @@
 
 #include "Resource.hpp"
 
-class ResourceManager:Resource
+class ResourceManager
 {
 public:
-	ResourceManager(){}
-	ResourceManager(const ResourceManager& RM){}
-	ResourceManager(ResourceManager&& RM) noexcept {}
-	ResourceManager operator=(const ResourceManager& RM) {}
-	ResourceManager operator=(ResourceManager&& RM) noexcept {}
-	~ResourceManager(){}
+  Resource *res;
+	ResourceManager(){res=new Resource;}
+	ResourceManager(const ResourceManager& RM){res=new Resource(*RM.res);}
+	ResourceManager(ResourceManager&& RM) noexcept {
+    res=RM.res;
+    RM.res=nullptr;
+    }
+	ResourceManager& operator=(const ResourceManager& RM);
+	ResourceManager& operator=(ResourceManager&& RM) noexcept;
+	~ResourceManager(){delete res;}
+  double get(){return res->get();}
 };
+
+ResourceManager & ResourceManager::operator=(const ResourceManager& RM)
+{
+    if(this!=&RM) *res=*RM.res;
+    return *this;
+}
+
+ResourceManager & ResourceManager::operator=(ResourceManager&& RM) noexcept
+{
+    if(this!=&RM){
+      delete res;
+      res=RM.res;
+      RM.res=nullptr;
+    }
+    return *this;
+  }
